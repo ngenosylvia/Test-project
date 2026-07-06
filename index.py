4ee30b1556a31a6204c56bb6cf32fd6900c72c8d
“Sylvia <ngenosylvia0@gmail.com>
2026-07-06 18:22:09 +0300

    Resolve merge conflict

--- index.py
+++ index.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-print ("Hello World!")print(Good evening)
-=======
 print ("Hello World!")
-print ("hello DSF-PT16!")
->>>>>>> e7dc02f4c8a85bb8c1dfa47642d2869cf33023f0
+print("Good evening")
+print ("Hello DSF-PT16!")
